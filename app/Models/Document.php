5909ab08f4c3af699cfed415d06de96453367e01
Peter Ayello Wright <peter.ayellowright@gmail.com>
2025-01-24 21:54:27 +0000

    set up the models

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Framework;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Document extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['title', 'description'];
+
+    public function frameworks(): HasMany
+    {
+        return $this->hasMany(Framework::class, 'document_framework');
+    }
+
+    public function skills(): HasMany
+    {
+        return $this->hasMany(Skill::class, 'document_skills');
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
---
+++ app/Models/Framework.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Document;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Framework extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function documents(): BelongsToMany
+    {
+        return $this->belongsToMany(Document::class, 'document_framework');
+    }
+}
